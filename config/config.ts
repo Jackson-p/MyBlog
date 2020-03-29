415b82135949c79803bf8d082e13775a14a42dd0
王乙评 <jackson@wangyipingdeMBP.lan>
2020-03-29 11:44:02 +0800

    feat: 第二版site

--- config/config.ts
+++ config/config.ts
@@ -3,8 +3,6 @@ const WorkboxPlugin = require("workbox-webpack-plugin");
 
 export default defineConfig({
   title: "Jackson",
-  favicon:
-    "https://graph.baidu.com/resource/121f5fdc086d8006746bc01585376233.jpg",
   base: "/",
   chainWebpack(memo) {
     memo.plugin("workbox").use(WorkboxPlugin.GenerateSW, [
